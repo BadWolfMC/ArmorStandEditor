992cf59e464081f21fb19e94e51b67747ce51b25
mercurialmusic <21208123+mercurialmusic@users.noreply.github.com>
2025-07-08 05:00:23 -0400

    Attempt Flint Fix

--- src/main/java/io/github/rypofalem/armorstandeditor/PlayerEditorManager.java
+++ src/main/java/io/github/rypofalem/armorstandeditor/PlayerEditorManager.java
@@ -224,14 +224,20 @@ public class PlayerEditorManager implements Listener {
 
   @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = false)
   void onRightClickTool(PlayerInteractEvent e) {
-    if (!(e.getAction() == Action.LEFT_CLICK_AIR ||
-        e.getAction() == Action.RIGHT_CLICK_AIR ||
-        e.getAction() == Action.LEFT_CLICK_BLOCK ||
-        e.getAction() == Action.RIGHT_CLICK_BLOCK)) return;
-    Player player = e.getPlayer();
-    if (!plugin.isEditTool(player.getInventory().getItemInMainHand())) return;
-    e.setCancelled(true);
-    getPlayerEditor(player.getUniqueId()).openMenu();
+      if (!(e.getAction() == Action.LEFT_CLICK_AIR ||
+            e.getAction() == Action.RIGHT_CLICK_AIR ||
+            e.getAction() == Action.LEFT_CLICK_BLOCK ||
+            e.getAction() == Action.RIGHT_CLICK_BLOCK)) return;
+
+      Player player = e.getPlayer();
+      if (!plugin.isEditTool(player.getInventory().getItemInMainHand())) return;
+
+      // Only proceed if the player is targeting an editable ArmorStand
+      Entity target = player.getTargetEntity(5);
+      if (target instanceof ArmorStand && canEdit(player, (ArmorStand) target)) {
+          e.setCancelled(true);
+          getPlayerEditor(player.getUniqueId()).openMenu();
+    }
   }
 
   @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
